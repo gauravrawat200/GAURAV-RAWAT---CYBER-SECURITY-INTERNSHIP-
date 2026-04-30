ae7fa79af6f491d87138bb60a2721b7102756b22
gauravrawat200 <rawatsatyam684@gmail.com>
2026-04-30 10:25:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from system_scan import system_info
+from suid_scan import suid_scan
+from permission_scan import permission_scan
+from cron_scan import cron_scan
+
+def run_all():
+    system_info()
+    suid_scan()
+    permission_scan()
+    cron_scan()
+
+if __name__ == "__main__":
+    run_all()
