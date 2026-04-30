12f9f303f630d05beaa29f62b004343b57f2e4bc
gauravrawat200 <rawatsatyam684@gmail.com>
2026-04-30 12:08:26 +0530

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from flask import Flask
-import threading
 import time
-
-from system_scan import system_info
-from suid_scan import suid_scan
-from permission_scan import permission_scan
-from cron_scan import cron_scan
-
-app = Flask(_name_)
 
 def run_all():
     system_info()
@@ -15,17 +6,8 @@ def run_all():
     permission_scan()
     cron_scan()
 
-def background_task():
+if _name_ == "_main_":
     while True:
         run_all()
         print("Running again in 60 seconds...")
         time.sleep(60)
-
-@app.route("/")
-def home():
-    return "Cyber Security Scanner is Running!"
-
-if _name_ == "_main_":
-    t = threading.Thread(target=background_task)
-    t.start()
-    app.run(host="0.0.0.0", port=10000)
