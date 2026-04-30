10ca55fbf3e6f1d103531f389e54fdda98322835
gauravrawat200 <rawatsatyam684@gmail.com>
2026-04-30 11:19:13 +0530

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import time
 
+from system_scan import system_info
+from suid_scan import suid_scan
+from permission_scan import permission_scan
+from cron_scan import cron_scan
+
+
 def run_all():
     system_info()
     suid_scan()
     permission_scan()
     cron_scan()
 
-if __name__ == "__main__":
+
+if _name_ == "_main_":
     while True:
         run_all()
         print("Running again in 60 seconds...")
